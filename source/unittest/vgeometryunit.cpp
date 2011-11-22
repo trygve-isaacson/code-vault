e4b9550836882bd7a7a522397aef5f642e45a2cc
Trygve Isaacson <trygve@bombaydigital.com>
2011-11-22 20:49:21 +0000

    VAULT-0004 [trygve] Update version strings to 3.3.

--- source/unittest/vgeometryunit.cpp
+++ source/unittest/vgeometryunit.cpp
@@ -1,6 +1,6 @@
 /*
 Copyright c1997-2011 Trygve Isaacson. All rights reserved.
-This file is part of the Code Vault version 3.2
+This file is part of the Code Vault version 3.3
 http://www.bombaydigital.com/
 */
 
